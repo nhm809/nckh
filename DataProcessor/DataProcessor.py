594552b4b4f8dd4a45f6c96ab9fd781f3fcb68c3
Nguyễn Hữu Mỹ <mynhse181827@fpt.edu.vn>
2025-03-12 11:00:16 +0700

    Update DataProcessor.py

--- DataProcessor/DataProcessor.py
+++ DataProcessor/DataProcessor.py
@@ -3,14 +3,31 @@ import pandas as pd
 file_path = "C:\\nckh\\DataSet\\StudentsPerformance_Updated.xlsx" // Đường dẫn tới file StudentsPerformance_Updated
                                                                     //Mỗi người sẽ có đường dẫn khác nhau. CẦN CHÚ Ý!!!
 
-df = pd.read_excel(file_path, sheet_name = "StudentsPerformance", index_col = "studentID")
+df = pd.read_excel(file_path, sheet_name = "StudentsPerformance")
 xls = pd.ExcelFile(file_path)
+
+# print(xls.sheet_names)
+
+# selected_df = df[['gender', 'math score','reading score', 'writing score']] df là các cột được chọn
 
 columns_to_drop = ['race/ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
 df_filtered = df.drop(columns=columns_to_drop)
 
+features = ["studentID", "gender", "math_score", "reading_score", "writing_score"]
+df_features = df[features].copy()  
+
+# Chuyển đổi cột "gender" thành số (Male = 1, Female = 0)  
+df_features["gender"] = df_features["gender"].map({"male": 1, "female": 0})  
+
+# Lưu dữ liệu đã chuẩn bị  
+df_features.to_csv("C:\\nckh\\DataSet\\Processed_StudentsPerformance.csv", index=False) 
+
 # df[['math_score', 'reading_score', 'writing_score']] /= 10
 
+# with pd.ExcelWriter(file_path, mode="a", engine="openpyxl", if_sheet_exists="replace") as writer:
+#     df.to_excel(writer, sheet_name="StudentsPerformance", index=True)  # Chỉ cập nhật sheet này
+    
+    
 # def get_recommendation(score):
 #     if 0 <= score <= 5:
 #         return "Poor"
